acdd3f8212a5b40146f96c75a661c93cb3851c37
wojtekwrona232 <48753496+wojtekwrona232@users.noreply.github.com>
2020-05-04 15:46:47 +0200

    new php file

--- gui/php/employees_table.php
+++ gui/php/employees_table.php
@@ -41,23 +41,18 @@
                         <td>Salary</td>
                         <td>Account</td>
                     </tr>
-                <?php
-                $conn = oci_connect('Wojtek', 'wojtek_wrona', '//localhost:1521/BazyDanychProjekt');
-                if (!$conn) {
-                    $e = oci_error();
-                    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-                }
-                $stid = oci_parse($conn, 'SELECT * FROM employees');
-                oci_execute($stid);
+                    <?php
+                    $stid = oci_parse($conn, 'SELECT * FROM employees');
+                    oci_execute($stid);
 
-                while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
-                    echo "<tr>\n";
-                    foreach ($row as $item) {
-                        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+                    while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+                        echo "<tr>\n";
+                        foreach ($row as $item) {
+                            echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+                        }
+                        echo "</tr>\n";
                     }
-                    echo "</tr>\n";
-                }
-                ?>
+                    ?>
                 </table>
             </div>
         </div>
